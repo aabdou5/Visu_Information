9cce4452ab4adbac9f9455e2348430b89ef86226
ybacomah <57348667+ybacomah@users.noreply.github.com>
2019-11-23 19:04:01 +0100

    DATA_Nettoyer

--- Survey_Students_MIASHS/data.R
+++ Survey_Students_MIASHS/data.R
@@ -1,9 +1,12 @@
 data <- read.csv2("results-survey111545.csv", sep = ";", header = TRUE)
-data <- data[ , c(1:2,8:24, 26,37:39, 42:47)] 
+data <- data[ , c(1:2,8:24, 26,38:42,46)] 
 length(names(data))
 names(data)[1]<-"ID_Reponse"
 row.names(data)=data$ID_Reponse
 data_Nettoyer<-data[,-1]
+names(data_Nettoyer) <- c("PROMOTION", "SEXE", "AGE", "LIEU_NAISSANCE", "VILLE_OBTENTION_BAC", "VILLE_OBTENTION_LICENCE", "VILLE_ALTERNANCE", "BAC", "ANNEE_BAC","EXPERIENCE_PRO_AVANT_MASTER",
+                          "RAISON_INSCRIPTION_INTERET-DISCIPLINE","RAISON_INSCRIPTION_PROJET-PRECIS" , "RAISON_INSCRIPTION_PROJET-PROFESSIONNEL" , "RAISON_INSCRIPTION_DEBOUCHES",
+                          "RAISON_INSCRIPTION_PROXIMITE-ETABLISSEMENT","SECTEUR_ACTIVITE", "TRANCHE_EFFECTIF_ENTREPRISE","APPARENCE_POSTE_OCCUPE","ROLE_ENTREPRISE","CHANCE_INSERTION_PROFESSIONNELLE_REGION-OCCITANIE")
 head(data_Nettoyer)
 edit(data_Nettoyer)
 
