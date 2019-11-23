ee849bdc960c838fd6aa1e770723d86211b1d6d5
anlimoua <43678580+anlimoua@users.noreply.github.com>
2019-11-23 12:35:01 +0100

    Add init R script

---
+++ Survey_Students_MIASHS/ui.R
@@ -0,0 +1,28 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+  
+  # Application title
+  titlePanel("Old Faithful Geyser Data"),
+  
+  # Sidebar with a slider input for number of bins 
+  sliderInput( inputId = "num"
+                 ,label = "Choose a number" 
+                 ,value = 25
+                 ,min = 1
+                 ,max = 100
+                )
+  , plotOutput("hist")
+    )
+  )
+
